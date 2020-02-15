8d12a9e7007cdb2064e981bd5a794f63f4655ca4
Jimena <jimenatorres.10@gmail.com>
2020-02-15 15:12:48 -0500

    Usando JavaScripti

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,16 +1,21 @@
 /*Implementar un algoritmo que reciba 2 argumentos y los sume,
  el resultado se debera imprimir en pantalla*/
-
-
- function suma (a,b) {
+function suma (a,b) {
      return a + b
  }
+console.log(suma (1,2));
 
+ /*Un estudiante realiza 4 examenes, calcular el promedio de estos*/
+function notas (a,b,c,d) {
+    return (a + b + c + d) / 4
+}
+console.log(notas (15,12,13,16));
  
-
- console.log(suma (1,2));
- 
-
+/*Calcular el area de un rectangulo*/
+function area (b,a) {
+    return b * a
+}
+console.log(area (7,3));
 
 
 
