802b75d57c487615556397f5c88b3b8f011cf534
Jimena <jimenatorres.10@gmail.com>
2020-02-15 15:00:11 -0500

    first commit

---
+++ Scripts/main.js
@@ -0,0 +1,17 @@
+/*Implementar un algoritmo que reciba 2 argumentos y los sume,
+ el resultado se debera imprimir en pantalla*/
+
+
+ function suma (a,b) {
+     return a + b
+ }
+
+ 
+
+ console.log(suma (1,2));
+ 
+
+
+
+
+
